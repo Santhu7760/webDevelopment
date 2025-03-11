4711e505566ea7491466ff18fbd50fad7a68ffee
Santhosh J R <137148886+Santhu7760@users.noreply.github.com>
2025-03-11 21:11:34 +0530

    Add files via upload

---
+++ JS/DOM/script.js
@@ -0,0 +1,23 @@
+//  let fC=document.firstChild;
+//  console.log("first child: ",fC);
+
+//  let fec=document.firstElementChild;
+//  console.log("first element child: ",fec);
+
+//  let lc=document.lastElementChild.firstElementChild;
+//  console.log("the last element child: ",lc);
+
+// let lists=document.querySelectorAll("li");
+
+
+// lists.forEach(element => {
+//     element.style.backgroundColor="black";
+//     element.style.padding="2rem";
+//     element.style.color="white";
+//     element.style.listStyle="none";
+//     element.style.textAlign="center";
+// });
+
+let a= Infinity;
+
+console.log(a);
